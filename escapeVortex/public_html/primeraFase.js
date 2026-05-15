447105276d0102254a66af91805ca926bafeb14b
PID <PID@DESKTOP-L47H0FO>
2026-05-15 09:49:06 +0200

    Comienzo de las instrucciones

--- escapeVortex/public_html/primeraFase.js
+++ escapeVortex/public_html/primeraFase.js
@@ -1,4 +1,5 @@
 "use_strict"; 
-NOMUSUARI=prompt("Introdueix el teu nom d'usuari (10 caràcters màxim):");
-benvinguda=alert("    🌌🌌🌌 BENVINGUT/DA "+NOMUSUARI+" A ESCAPE VORTEX!!! 🌌🌌🌌");
+const NOMUSUARI=prompt("Introduce un nombre de usuario (10 carácteres como máximo):");
+let benvinguda=alert("🌌🌌🌌 Bienvenido/a comandante ✨"+NOMUSUARI.substring(0,9)+"✨ a Escape Vortex!!! 🌌🌌🌌");
+let instruccions=alert("🚨 MISIÓN ESPACIAL INICIADA 🚨\nBienvenido/a comandante "+NOMUSUARI+". Has sido enviado a una estación espacial abandonada situada cerca de un vortex dimensional. Un fallo en el sistema principal ha provocado que todos los portales de salida queden bloqueados y ahora tu única esperanza es activarlos manualmente resolviendo los desafíos del sistema.");
 
