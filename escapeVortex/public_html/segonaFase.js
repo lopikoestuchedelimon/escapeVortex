ad62e1c96ebc5157998f4bcc3042234886ccd264
PID <PID@DESKTOP-OBA55DU>
2026-05-15 09:49:24 +0200

    continuar fase 2

--- escapeVortex/public_html/segonaFase.js
+++ escapeVortex/public_html/segonaFase.js
@@ -1,2 +1,4 @@
 "use_strict";
-const preguntes=[""]
+const preguntes=["1.Tiene ojos pero no puede ver nada, tiene piel pero no siente el frío ¿Qué es?\n A) Una estatua de mármol.\n B) Una patata\n C) Un abrigo de cuero",
+    "\n2.Vas corriendo en una carrera de velocidad y adelantas a la persona que va en el segundo lugar. ¿En qué posición estás ahora?\nA) Primero \nB) Segundo\nC) Tercero"];
+alert(preguntes);
