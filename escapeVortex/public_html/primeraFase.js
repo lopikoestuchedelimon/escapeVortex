ce0b8a7f99ce4dfbb9d3437321f3984748d1efba
PID <PID@DESKTOP-L47H0FO>
2026-05-15 09:21:48 +0200

    Título y nombre de usuario, holi

--- escapeVortex/public_html/primeraFase.js
+++ escapeVortex/public_html/primeraFase.js
@@ -1,4 +1,4 @@
 "use_strict"; 
 NOMUSUARI=prompt("Introdueix el teu nom d'usuari (10 caràcters màxim):");
-benvinguda=alert("BENVINGUT/DA "+NOMUSUARI+" ")
+benvinguda=alert("    🌌🌌🌌 BENVINGUT/DA "+NOMUSUARI+" A ESCAPE VORTEX!!! 🌌🌌🌌");
 
