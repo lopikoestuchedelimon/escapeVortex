4d21ed51a13854f9371a7b9325c028b2761bddcc
PID <PID@DESKTOP-OBA55DU>
2026-05-20 11:50:26 +0200

    subir y continuar fase 2

--- escapeVortex/public_html/segonaFase.js
+++ escapeVortex/public_html/segonaFase.js
@@ -1,4 +1,43 @@
 "use_strict";
-const preguntes=["1.Tiene ojos pero no puede ver nada, tiene piel pero no siente el frío ¿Qué es?\n A) Una estatua de mármol.\n B) Una patata\n C) Un abrigo de cuero",
-    "\n2.Vas corriendo en una carrera de velocidad y adelantas a la persona que va en el segundo lugar. ¿En qué posición estás ahora?\nA) Primero \nB) Segundo\nC) Tercero"];
-alert(preguntes);
+// Variables i constants
+        const PREGUNTES = 11;
+        let intents = 3;
+        let comodins = 3;
+   
+if (intents == 0) {
+alert("DEERROTA");
+} else if (PREGUNTES == 11) {
+alert("VICTORIA");
+}
+
+//preguntes
+/* pregunta: "¿Qué número falta? 2, 4, 6, __",
+ opcions: ["a) 8", "b) 10", "c) 12"],
+ correcta: "a"
+ 
+ pregunta: "Si 5 + 7 × 2 = ¿?",
+ opcions: ["a) 24", "b) 19", "c) 14"],
+ correcta: "b"
+ 
+ pregunta: "Enigma: Tengo llaves però no puedo abrir puertas. ¿Qué soy?",
+ opcions: ["a) Un piano", "b) Un coche", "c) Una casa"],
+ correcta: "a"
+ 
+ pregunta: "¿Cuánto es 9 × 9?",
+ opcions: ["a) 72", "b) 81", "c) 99"],
+ correcta: "b"
+ 
+ pregunta: "Enigma: Cuanto más me quites, más grande me hago. ¿Qué soy?",
+ opcions: ["a) Un agujero", "b) Un árbol", "c) Un libro"],
+ correcta: "a"
+ 
+ pregunta:"Tiene ojos pero no puede ver nada, tiene piel pero no siente el frío ¿Qué es?"
+ opcions: ["a) Una estatua de mármol", "b) Una patata", "c) Un abrigo de cuero"]
+ correcta: "b"
+ 
+ pregunta: "Vas corriendo en una carrera de velocidad y adelantas a la persona que va en el segundo lugar. ¿En qué posición estás ahora?"
+ opcions: [a) Primero","b) Segundo","c) Tercero"]
+ correcta: "b"
+ 
+ 
+*/
