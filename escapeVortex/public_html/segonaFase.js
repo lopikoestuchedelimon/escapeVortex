58595b7635df17aede64d7144fa7a986c9d6c8e3
PID <PID@DESKTOP-L47H0FO>
2026-05-25 13:06:26 +0200

    preguntas hechas

--- escapeVortex/public_html/segonaFase.js
+++ escapeVortex/public_html/segonaFase.js
@@ -1,5 +1,15 @@
 "use_strict";
 // Variables i constants
+const PREGUNTES1=["Qué número falta? 2, 4, 16, __. Opcions: \na) 8\nb) 10\nc) 12",
+    "Si 5 + 7 × 2 = ¿? Opcions: \na) 24 \nb) 19 \nc) 14",
+    "Enigma: Tengo llaves però no puedo abrir puertas. ¿Qué soy? Opcions: \na) Un piano \nb) Un coche \nc) Una casa",
+    "¿Cuánto es 9 × 9? Opcions: \na) 72 \nb) 81 \nc) 99",
+    "Enigma: Cuanto más me quites, más grande me hago. ¿Qué soy? Opcions: \na) Un agujero \nb) Un árbol \nc) Un libro",
+    "Tiene ojos pero no puede ver nada, tiene piel pero no siente el frío ¿Qué es? Opcions: \na) Una estatua de mármol \nb) Una patata \nc) Un abrigo de cuero",
+    "Vas corriendo en una carrera de velocidad y adelantas a la persona que va en el segundo lugar. ¿En qué posición estás ahora? Opcions: \na) Primero \nb) Segundo \nc) Tercero",
+    "Tengo una cola brillante pero no soy un animal. Viajo por el espacio profundo y me deshago lentamente cuando me acerco al Sol. ¿Qué soy? \na) Un asteroide \nb) Un cometa \nc) Un cohete",
+"Soy un lugar del espacio donde la gravedad es tan invisible como destructiva. Absorbo todo lo que se me acerca demasiado, y ni siquiera la luz puede escapar de mis manos. ¿Qué soy? \na) Un agujero negro \nb) Una estrella de neutrones \n) La cara oculta de la Luna",
+"No tengo luz propia, pero brillo en la noche. Cambio de forma cada semana, pero siempre soy la misma. ¿Qué soy? \na) Una estrella lejana. \nb) El planeta Marte. \nc) La Luna"];
         const PREGUNTES = 11;
         const RESPOSTES=["a","b","a","b","a","b","b","a","a","a"];
         let intents = 3;
