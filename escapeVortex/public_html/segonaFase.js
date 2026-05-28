ea49e03b3918e49f37d703a689e9f1bebecc4ad7
PID <PID@DESKTOP-OBA55DU>
2026-05-28 13:02:33 +0200

    Subir parte de la fase 2 "acabada"

--- escapeVortex/public_html/segonaFase.js
+++ escapeVortex/public_html/segonaFase.js
@@ -1,41 +1,38 @@
 "use_strict";
 // Variables i constants
+
+const PREGUNTES1=["a","b","c","d","e","f","g","h","i","j"];
+const RESPUESTAS=["a","b","c","d","e","f","g","h","i","j"];
         const PREGUNTES = 11;
-        let intents = 3;
-        let comodins = 3;
-   
-if (intents == 0) {
+        
+let intentos = 3;
+let comodines = 3;
+let i = 0;
+
+do {
+    let usuario = prompt(PREGUNTES1[i]);
+    if (usuario == RESPUESTAS[i]) {
+        alert("Respuesta correcta!!!");
+        i++;
+} else {
+        alert("Respuesta incorrecta");
+        let usarComodines = prompt("¿Quieres acceder a un portal de emergencia para escapar del enigma?");
+        if (usarComodines=="si" && comodines > 0) {
+            comodines--;
+            alert("Has accedido a un portal de emergencia. Te quedan: " + comodines);
+            i++;
+        } else {
+
+            intentos--;
+            alert("Has perdido un intento, NO TE RINDAS!!!. Te quedan: " + intentos);
+
+        }
+    }
+
+} while (i < PREGUNTES1.length && intentos > 0);
+
+if (intentos == 0) {
 alert("DEERROTA");
 } else if (PREGUNTES == 11) {
 alert("VICTORIA");
 }
-
-//preguntes
-/* pregunta: "¿Qué número falta? 2, 4, 6, __",
- opcions: ["a) 8", "b) 10", "c) 12"],
- correcta: "a"
- 
- pregunta: "Si 5 + 7 × 2 = ¿?",
- opcions: ["a) 24", "b) 19", "c) 14"],
- correcta: "b"
- 
- pregunta: "Enigma: Tengo llaves però no puedo abrir puertas. ¿Qué soy?",
- opcions: ["a) Un piano", "b) Un coche", "c) Una casa"],
- correcta: "a"
- 
- pregunta: "¿Cuánto es 9 × 9?",
- opcions: ["a) 72", "b) 81", "c) 99"],
- correcta: "b"
- 
- pregunta: "Enigma: Cuanto más me quites, más grande me hago. ¿Qué soy?",
- opcions: ["a) Un agujero", "b) Un árbol", "c) Un libro"],
- correcta: "a"
- 
- pregunta:"Tiene ojos pero no puede ver nada, tiene piel pero no siente el frío ¿Qué es?"
- opcions: ["a) Una estatua de mármol", "b) Una patata", "c) Un abrigo de cuero"]
- correcta: "b"
- 
- pregunta: "Vas corriendo en una carrera de velocidad y adelantas a la persona que va en el segundo lugar. ¿En qué posición estás ahora?"
- opcions: [a) Primero","b) Segundo","c) Tercero"]
- correcta: "b"
-*/
