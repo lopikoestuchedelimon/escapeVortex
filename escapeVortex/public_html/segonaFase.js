719955664ad6f54c015b1b98b53a5bb7c84957e3
PID <PID@DESKTOP-L47H0FO>
2026-05-28 13:06:18 +0200

    preguntas completas y ajustadas

--- escapeVortex/public_html/segonaFase.js
+++ escapeVortex/public_html/segonaFase.js
@@ -3,53 +3,22 @@
 const PREGUNTES1=["Qué número falta? 2, 4, 16, __. Opcions: \na) 8\nb) 10\nc) 12",
     "Si 5 + 7 × 2 = ¿? Opcions: \na) 24 \nb) 19 \nc) 14",
     "Enigma: Tengo llaves però no puedo abrir puertas. ¿Qué soy? Opcions: \na) Un piano \nb) Un coche \nc) Una casa",
-    "¿Cuánto es 9 × 9? Opcions: \na) 72 \nb) 81 \nc) 99",
-    "Enigma: Cuanto más me quites, más grande me hago. ¿Qué soy? Opcions: \na) Un agujero \nb) Un árbol \nc) Un libro",
+    "¿Cuánto es 9 × 9? Opcions: \na) 72 \nb) 99 \nc) 81",
+    "Enigma: Cuanto más me quites, más grande me hago. ¿Qué soy? Opcions: \na) Un libro \nb) Un árbol \nc) Un agujero",
     "Tiene ojos pero no puede ver nada, tiene piel pero no siente el frío ¿Qué es? Opcions: \na) Una estatua de mármol \nb) Una patata \nc) Un abrigo de cuero",
     "Vas corriendo en una carrera de velocidad y adelantas a la persona que va en el segundo lugar. ¿En qué posición estás ahora? Opcions: \na) Primero \nb) Segundo \nc) Tercero",
     "Tengo una cola brillante pero no soy un animal. Viajo por el espacio profundo y me deshago lentamente cuando me acerco al Sol. ¿Qué soy? \na) Un asteroide \nb) Un cometa \nc) Un cohete",
-"Soy un lugar del espacio donde la gravedad es tan invisible como destructiva. Absorbo todo lo que se me acerca demasiado, y ni siquiera la luz puede escapar de mis manos. ¿Qué soy? \na) Un agujero negro \nb) Una estrella de neutrones \n) La cara oculta de la Luna",
+"Soy un lugar del espacio donde la gravedad es tan invisible como destructiva. Absorbo todo lo que se me acerca demasiado, y ni siquiera la luz puede escapar de mis manos. ¿Qué soy? \na) La cara oculta de la luna \nb) Una estrella de neutrones \n) Un agujero negro",
 "No tengo luz propia, pero brillo en la noche. Cambio de forma cada semana, pero siempre soy la misma. ¿Qué soy? \na) Una estrella lejana. \nb) El planeta Marte. \nc) La Luna"];
         const PREGUNTES = 11;
-        const RESPOSTES=["a","b","a","b","a","b","b","a","a","a"];
-        let intents = 3;
-        let comodins = 3;
-   
-if (intents == 0) {
-alert("DEERROTA");
+        const RESPOSTES=["a","b","a","c","c","b","b","a","c","a"];
+let intentos = 3;
+let comodines = 3;
+let i = 0;
+
+if (intentos == 0) {
+alert("Misión fallida 😞, unos aliens te han lanzado un rayo hipersónico y te han desintegrado al instante. 🛸👽");
 } else if (PREGUNTES == 11) {
-alert("VICTORIA");
+alert("¡¡¡🎉Bien hecho🎉!!! Has completado la mision con éxito, toma tu premio 🤲🍔");
 }
 
-
-//preguntes
-/* pregunta: "¿Qué número falta? 2, 4, 6, __",
- opcions: ["a) 8", "b) 10", "c) 12"],
- correcta: "a"
- 
- pregunta: "Si 5 + 7 × 2 = ¿?",
- opcions: ["a) 24", "b) 19", "c) 14"],
- correcta: "b"
- 
- pregunta: "Enigma: Tengo llaves però no puedo abrir puertas. ¿Qué soy?",
- opcions: ["a) Un piano", "b) Un coche", "c) Una casa"],
- correcta: "a"
- 
- pregunta: "¿Cuánto es 9 × 9?",
- opcions: ["a) 72", "b) 81", "c) 99"],
- correcta: "b"
- 
- pregunta: "Enigma: Cuanto más me quites, más grande me hago. ¿Qué soy?",
- opcions: ["a) Un agujero", "b) Un árbol", "c) Un libro"],
- correcta: "a"
- 
- pregunta:"Tiene ojos pero no puede ver nada, tiene piel pero no siente el frío ¿Qué es?"
- opcions: ["a) Una estatua de mármol", "b) Una patata", "c) Un abrigo de cuero"]
- correcta: "b"
- 
- pregunta: "Vas corriendo en una carrera de velocidad y adelantas a la persona que va en el segundo lugar. ¿En qué posición estás ahora?"
- opcions: [a) Primero","b) Segundo","c) Tercero"]
- correcta: "b"
- 
- 
-*/
